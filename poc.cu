e28dd918a616f6aa22cab8c61b227cb03e84fc22
Andy Polyakov <appro@cryptogams.org>
2024-07-03 12:09:02 +0200

    Pass sppark-style errors.

--- poc.cu
+++ poc.cu
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 __global__ void kernel()
 {
     printf("hello from GPU\n");
 }
+
+struct Error {
+    int code;
+    char *message;
+};
 
 extern "C"
 #ifdef _WIN32
@@ -11,8 +17,12 @@ __declspec(dllexport)
 #else
 __attribute__((visibility("default")))
 #endif
-void cuda_call()
+Error cuda_call()
 {
     kernel<<<1,1>>>();
-    cudaDeviceSynchronize();
+    auto err = cudaGetLastError();
+    if (err != cudaSuccess)
+        return {err, strdup(cudaGetErrorString(err))};
+    err = cudaDeviceSynchronize();
+    return {err, strdup(cudaGetErrorString(err))};
 }
--- poc.go
+++ poc.go
@@ -8,9 +8,31 @@ package main
 // # include <stdlib.h>
 // #endif
 //
-// static void (*cuda_call)();
+// typedef struct {
+//     int code;
+//     char *message;
+// } Error;
 //
-// static void bridge()
+// static Error (*cuda_call)();
+//
+// typedef struct {
+//     int code;
+//     _GoString_ message;
+// } GoError;
+//
+// void toGoString(_GoString_ *, char *);
+//
+// static void toGoError(GoError *go_err, Error c_err)
+// {
+//     go_err->code = c_err.code;
+//     if (c_err.message != NULL) {
+//         toGoString(&go_err->message, c_err.message);
+//         free(c_err.message);
+//         c_err.message = NULL;
+//     }
+// }
+//
+// static void bridge(GoError *go_err)
 // {
 //     if (cuda_call == NULL) {
 // #ifdef _WIN32
@@ -31,10 +53,19 @@ package main
 // #endif
 //     }
 //
-//     if (cuda_call) cuda_call();
+//     if (cuda_call) toGoError(go_err, cuda_call());
 // }
 import "C"
 
+//export toGoString
+func toGoString(go_str *string, c_str *C.char) {
+    *go_str = C.GoString(c_str)
+}
+
 func main() {
-    C.bridge()
+    var err C.GoError
+    C.bridge(&err)
+    if err.code != 0 {
+        panic(err.message)
+    }
 }
